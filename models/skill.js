f84a9b2ad1f7948d3e0abe3f715a277a53b62d4f
Amanda Stern <amanda.stern.73@gmail.com>
2022-09-22 14:58:23 -0400

    export Skill Schema

--- models/skill.js
+++ models/skill.js
@@ -4,4 +4,11 @@ const Schema = mongoose.Schema
 
 const skillSchema = new Schema ({
   text: String,
+  done: Boolean,
 })
+
+const Skill = mongoose.model('Skill', skillSchema)
+
+export {
+  Skill
+}
